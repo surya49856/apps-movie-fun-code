e0ea4d19ed272b4248410dd1f94b2f6d53261cc8
Charitha Thota <tvlcharitha@gmail.com>
2018-03-01 08:41:07 -0700

    movie fun

--- src/main/java/org/superbiz/moviefun/albums/AlbumFixtures.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumFixtures.java
@@ -3,11 +3,9 @@ package org.superbiz.moviefun.albums;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
 import static com.fasterxml.jackson.dataformat.csv.CsvSchema.ColumnType.NUMBER;
+import org.springframework.stereotype.Component;
+import java.util.List;
 import static org.superbiz.moviefun.CsvUtils.readFromCsv;
 
 @Component
@@ -16,6 +14,7 @@ public class AlbumFixtures {
     private final ObjectReader objectReader;
 
     public AlbumFixtures() {
+
         CsvSchema schema = CsvSchema.builder()
             .addColumn("artist")
             .addColumn("title")
